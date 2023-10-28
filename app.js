646ce63f25e9e60d4afd4ac155b84dd6905fb116
Ruma303 <matteoh8@hotmail.it>
2023-10-28 10:23:40 +0200

    errori con eventi

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
 const fs = require('fs');
-console.log(fs);
+//console.log(fs);
 
+/* console.log(fs.existsSync('app.js'));
+
+console.log(fs.statSync('app.js')); */
+
+
+
+//% Operazioni con il modulo fs
+
+//, Lettura
+
+//# async
+    /* fs.readFile('testo.tx', 'utf-8', function(err, data) {
+        if (err) {
+            //console.log(err);
+            return handleError(new Error(err));
+        } else {
+            console.log(data);
+        }
+    });
+
+    function handleError(err) {
+        console.log(`Errore catturato: ${err}`);
+    } */
+
+// Con eventi
+
+    const { EventEmitter } = require('events');
+    const eve = new EventEmitter;
+    fs.readFile('t.txt', 'utf-8', (err, data) => {
+        if (err) {
+            return eve.emit('error', err);
+        }
+        console.log(err, data);
+    });
+    eve.on('error', eventErr => console.log(eventErr));
+
+//# sync
+
+    data = fs.readFileSync('testo.txt', 'utf-8')
+//, Scrittura

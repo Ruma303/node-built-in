eb0aa58e19404fe5a72ab3e14e1f7433baa12b59
Ruma303 <matteoh8@hotmail.it>
2023-10-28 10:42:27 +0200

    lettura cartelle

--- app.js
+++ app.js
@@ -12,32 +12,66 @@ console.log(fs.statSync('app.js')); */
 //, Lettura
 
 //# async
-    /* fs.readFile('testo.tx', 'utf-8', function(err, data) {
-        if (err) {
-            //console.log(err);
-            return handleError(new Error(err));
-        } else {
-            console.log(data);
-        }
-    });
+/* fs.readFile('testo.tx', 'utf-8', function(err, data) {
+    if (err) {
+        //console.log(err);
+        return handleError(new Error(err));
+    } else {
+        console.log(data);
+    }
+});
 
-    function handleError(err) {
-        console.log(`Errore catturato: ${err}`);
-    } */
+function handleError(err) {
+    console.log(`Errore catturato: ${err}`);
+} */
 
 // Con eventi
 
-    const { EventEmitter } = require('events');
-    const eve = new EventEmitter;
-    fs.readFile('t.txt', 'utf-8', (err, data) => {
-        if (err) {
-            return eve.emit('error', err);
-        }
-        console.log(err, data);
-    });
-    eve.on('error', eventErr => console.log(eventErr));
+/* const { EventEmitter } = require('events');
+const eve = new EventEmitter;
+fs.readFile('t.txt', 'utf-8', (err, data) => {
+    if (err) {
+        return eve.emit('error', err);
+    }
+    console.log(err, data);
+});
+eve.on('error', eventErr => console.log(eventErr));
+ */
+
 
 //# sync
+/* try {
+    data = fs.readFileSync('testo.txt', 'utf-8');
+    console.log(data);
+} catch (err) {
+    console.log(err);
+} */
 
-    data = fs.readFileSync('testo.txt', 'utf-8')
+
+//# lettura di cartelle
+
+    dir = '.\\cartella';
+    fs.readdir(dir, (err, files) => {
+
+        if (err) {
+            console.log(err);
+        } else {
+
+            for (content of files) {
+                stat = fs.statSync(dir + '\\' + content);
+
+                if (stat.isDirectory()) {
+                    console.log(`(dir) ${content}`);
+                } else {
+                    console.log(`${content} (${stat.size} bytes)`);
+                }
+            }
+        }
+    });
+
+
+
+
+
+
 //, Scrittura
